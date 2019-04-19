eff20705390fc4d54b52b21461fe4e03ee8d075d
zoewithabang <zoewithabang@gmail.com>
2019-04-19 11:14:30 +0100

    Initial proof of concept, pulling Users from DB

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,72 @@
 extern crate dotenv;
+extern crate mysql;
 
 use dotenv::dotenv;
 use std::env;
 
+#[derive(PartialEq)]
+enum MarkovType {
+    Single,
+    Mashup,
+    Server
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct User {
+    id: String,
+    tracked: u8,
+    permission_rank: u8
+}
+
 fn main() {
+    const MESSAGE_COUNT: u32 = 200_000;
+    const MAX_LENGTH: u32 = 15;
+    const MARKOV_TYPE: MarkovType = MarkovType::Server;
+
+    let user_ids: Vec<String> = vec!["83126901098414080".to_string(),
+                                     "83132676634054656".to_string(),
+                                     "83357928362344448".to_string(),
+                                     "83739773033775104".to_string(),
+                                     "84246523071725568".to_string(),
+                                     "89718706225041408".to_string()];
+
     dotenv().ok();
 
-    for(key, value) in env::vars() {
-        println!("{}: {}", key, value);
+    let messages = get_stored_messages(user_ids, MESSAGE_COUNT, MARKOV_TYPE);
+}
+
+fn get_stored_messages(user_ids: Vec<String>, message_count: u32, markov_type: MarkovType) /*-> Vec<String>*/ {
+    match markov_type {
+        MarkovType::Single => println!("Single"),
+        MarkovType::Mashup => println!("Mashup"),
+        MarkovType::Server => println!("Server"),
+    };
+
+    let db = format!("mysql://{}:{}@{}:{}/{}",
+        env::var("dbuser").unwrap(),
+        env::var("dbpassword").unwrap(),
+        env::var("dbaddress").unwrap(),
+        env::var("dbport").unwrap(),
+        env::var("dbdatabase").unwrap());
+
+    println!("{}", db);
+
+    let pool = mysql::Pool::new(db).unwrap();
+
+    let users = pool.prep_exec("SELECT * from users", ())
+        .map(|result| {
+            result.map(|x| x.unwrap())
+                .map(|row| {
+                    let (id, tracked, permission_rank) = mysql::from_row(row);
+                    User {
+                        id,
+                        tracked,
+                        permission_rank
+                    }
+                }).collect::<Vec<User>>()
+        }).unwrap();
+
+    for user in users {
+        println!("{:?}", user);
     }
 }
